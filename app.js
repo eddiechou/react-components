8222780bd02a60fc1ef2b9a9bee33c883145b9ce
Eddie Chou <eddieechou@gmail.com>
2017-02-22 11:51:09 -0800

    Made a grocery list using a React component consisting of GroceryListItem Components

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+var GroceryListComponent = (props) => (
+  <ul>
+    {props.items.map(item => 
+      <GroceryListItem item={item}/>
+    )}
+  </ul>
+);
+
+{ /*}
+var ApplesComponent = (props) => (
+  <li>Apples</li>
+);
+
+var OrangesComponent = () => (
+  <li>Oranges</li>
+);
+*/ }
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bold: false
+    };
+
+  }
+
+  onListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item} </li>
+    );
+  }
+}
+
+
+ReactDOM.render(<GroceryListComponent items={['apples', 'oranges']} />, document.getElementById('app'));
